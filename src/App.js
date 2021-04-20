231779a32cd1659adfe3f0d833209d0fb2d21194
kelvin9314 <kelvin9314@gmail.com>
2021-04-20 17:56:26 +0800

    single marker

--- src/App.js
+++ src/App.js
@@ -3,25 +3,7 @@ import './App.css'
 import Map from './views/Map'
 
 function App() {
-  return (
-    <Map />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  )
+  return <Map />
 }
 
 export default App
--- src/views/Map.js
+++ src/views/Map.js
@@ -1,5 +1,9 @@
-import { useEffect } from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import fetch from 'unfetch'
+import useSWR from 'swr'
+
+const fetcher = url => fetch(url).then(r => r.json())
 
 const containerStyle = {
   width: '100%',
@@ -7,27 +11,78 @@ const containerStyle = {
   height: '33.33em',
 }
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
+// TAG Taipei
+const defaultCenter = {
+  lat: 25.047924,
+  lng: 121.517081,
 }
 
-const Map = Props => {
-  useEffect(() => {
-    // console.log(process.env.REACT_APP_GOOGLE_MAPS_KEY)
+const position = {
+  lat: 25.047924,
+  lng: 121.517081,
+}
+
+function MyComponent() {
+  // ?type=2
+  //
+  const { data: bikeStations } = useSWR(' https://apis.youbike.com.tw/api/front/station/all', fetcher)
+  console.log(bikeStations)
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
+    language: 'zh-TW', // 'en', 'zh-TW', https://developers.google.com/maps/faq#languagesupport
+    // version: '3',
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds()
+    map.fitBounds(bounds)
+    setMap(map)
   }, [])
 
-  return (
-    <div style={{ display: 'grid', justifyItems: 'center' }}>
-      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-          {/* Child components, such as markers, info windows, etc. */}
-          <></>
-        </GoogleMap>
-        <div></div>
-      </LoadScript>
-    </div>
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  const onLoadMarker = marker => {
+    console.log('marker: ', marker)
+  }
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>
+  }
+
+  return isLoaded ? (
+    <>
+      {/* <select>
+        <option>English</option>
+        <option>中文</option>
+      </select> */}
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={defaultCenter}
+        zoom={13}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        {/* {bikeStations?.retVal?.length > 1 &&
+          bikeStations?.retVal.forEach(bikeStations => {
+            // console.log(bikeStations)
+            const { lat, lng } = bikeStations
+            console.log(lat, lng)
+            return <Marker onLoad={onLoadMarker} position={{ lat, lng }} />
+          })} */}
+        <Marker onLoad={onLoadMarker} position={position} />
+        <></>
+      </GoogleMap>
+    </>
+  ) : (
+    <></>
   )
 }
 
-export default Map
+export default React.memo(MyComponent)
